37ccc6db13ee214522443b5e0860b9970e0da520
Pham Ngoc Son <phamngocson6596@gmail.com>
2023-03-13 17:15:24 +0700

    a

--- qnhu/index.js
+++ qnhu/index.js
@@ -4,5 +4,6 @@ const tablehtml = document.querySelector("#frameTable")
 listImage.sort(() => Math.random() - 0.5);
 
 listImage.forEach(img=>{
-    tablehtml.insertAdjacentHTML("beforeend", `<div class="w3-panel w3-center"><img src="${img}" alt="qnhu" class="w3-card-4"></div>`)
+    tablehtml.insertAdjacentHTML("beforeend", 
+    `<div class="w3-panel w3-center"><img src="${img}" alt="qnhu" class="w3-card-4"></div>`)
 })
