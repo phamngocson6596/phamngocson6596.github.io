a14226e6246cf46e258dcd77dff92a0ff4fed73b
Pham Ngoc Son <phamngocson6596@gmail.com>
2023-04-22 11:10:32 +0700

    123

--- notary/phicongchung/new.js
+++ notary/phicongchung/new.js
@@ -6,8 +6,59 @@ hsBtn.addEventListener("click", ()=>{
     document.getElementById("id02").style.display = "block";
 })
 // Add event listener to the button
-addBtn.addEventListener('click', () => {
-  // Get the "Danh mục hồ sơ" element
+// addBtn.addEventListener('click', () => {
+//   // Get the "Danh mục hồ sơ" element
+//   const danhMucHoSo = document.querySelector('.w3-container');
+
+//   // Create a new "hoso" element
+//   const hosoElement = document.createElement('div');
+//   hosoElement.classList.add('w3-card', 'hoso');
+//   hosoElement.style.marginBottom = `15px`;
+
+//   const hosoRow = document.createElement('div');
+//   hosoRow.classList.add('w3-cell-row');
+
+//   const hosoText = document.createElement('div');
+//   hosoText.classList.add('w3-cell');
+//   hosoText.innerHTML = '<p class="w3-padding-large">Hợp đồng mua bán</p>';
+
+//   const hosoCloseBtn = document.createElement('div');
+//   hosoCloseBtn.classList.add('w3-cell', 'w3-button', 'close-hoso');
+//   hosoCloseBtn.style.width = '10%';
+//   hosoCloseBtn.innerHTML = '<i class="fas fa-times"></i>';
+
+//   hosoRow.appendChild(hosoText);
+//   hosoRow.appendChild(hosoCloseBtn);
+//   hosoElement.appendChild(hosoRow);
+
+//   // Add the new "hoso" element to the "Danh mục hồ sơ" element
+//   danhMucHoSo.appendChild(hosoElement);
+
+//   // Attach event listener to the new "close-hoso" button
+//   hosoCloseBtn.addEventListener('click', () => {
+//     // Get the parent "hoso" element
+//     const parentHosoElement = hosoCloseBtn.parentNode.parentNode;
+
+//     // Remove the "hoso" element
+//     parentHosoElement.remove();
+//   });
+// });
+
+// Get all the "close-hoso" button elements
+const closeBtns = document.querySelectorAll('.close-hoso');
+
+// Add event listener to all the "close-hoso" buttons
+closeBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    // Get the "hoso" element
+    const hosoElement = btn.parentNode.parentNode;
+
+    // Remove the "hoso" element
+    hosoElement.remove();
+  });
+});
+
+function addNewHoso(tenhoso) {
   const danhMucHoSo = document.querySelector('.w3-container');
 
   // Create a new "hoso" element
@@ -20,7 +71,7 @@ addBtn.addEventListener('click', () => {
 
   const hosoText = document.createElement('div');
   hosoText.classList.add('w3-cell');
-  hosoText.innerHTML = '<p class="w3-padding-large">Hợp đồng mua bán</p>';
+  hosoText.innerHTML = `<p class="w3-padding-large">${tenhoso}</p>`;
 
   const hosoCloseBtn = document.createElement('div');
   hosoCloseBtn.classList.add('w3-cell', 'w3-button', 'close-hoso');
@@ -42,18 +93,4 @@ addBtn.addEventListener('click', () => {
     // Remove the "hoso" element
     parentHosoElement.remove();
   });
-});
-
-// Get all the "close-hoso" button elements
-const closeBtns = document.querySelectorAll('.close-hoso');
-
-// Add event listener to all the "close-hoso" buttons
-closeBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    // Get the "hoso" element
-    const hosoElement = btn.parentNode.parentNode;
-
-    // Remove the "hoso" element
-    hosoElement.remove();
-  });
-});
+}
