df0f3e5b82da12123d837fd6ce6d3181aac95c50
phamngocson6596 <phamngocson6596@gmail.com>
2024-03-17 09:58:27 +0700

    123

--- notary/qrcode/script.js
+++ notary/qrcode/script.js
@@ -1,7 +1,8 @@
 document.getElementById("sendRequest").addEventListener("click", async () => {
-  const QRcode = document.getElementById("QRcode").value;
+  const inputtext = document.getElementById("QRcode").value;
+  const qrcodes = datahandler(inputtext);
   const postData = {
-    QRcode,
+    qrcodes,
     CreatedDate: new Date(),
     AccessTime: new Date(),
     Creator: "Tama",
@@ -25,53 +26,28 @@ document.getElementById("sendRequest").addEventListener("click", async () => {
       console.log("Response:", data);
     } else {
       const message = await response.text();
-      console.error("Failed to make POST request:", message);
+      const data = await response.json();
+      console.error("Failed to make POST request:", `${message}: ${data}`);
     }
   } catch (error) {
     console.error("Error:", error.message);
   }
 });
 
-// document
-//   .getElementById("myForm")
-//   .addEventListener("submit", async function (event) {
-//     event.preventDefault(); // Prevent default form submission
-
-//     // Get form data
-//     const formDataJson = Object.fromEntries(new FormData(this).entries());
-//     const formData = {
-//       ...formDataJson,
-//       CreatedDate: new Date(),
-//       AccessTime: new Date(),
-//       Creator: "Tama",
-//     };
-//     console.log(formData);
-//     // Execute your custom logic for handling the form submission
-//     await handleFormSubmission(formData);
-//   });
-
-// async function handleFormSubmission(formData) {
-//   try {
-//     const response = await fetch(
-//       "https://asia-east2-dataconnecting-a1a50.cloudfunctions.net/app/users",
-//       {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: formData,
-//       }
-//     );
-
-//     if (response.ok) {
-//       console.log("POST request successful");
-//       const data = await response.json();
-//       console.log("Response:", data);
-//     } else {
-//       const message = await response.text();
-//       console.error("Failed to make POST request:", message);
-//     }
-//   } catch (error) {
-//     console.error("Error:", error.message);
-//   }
-// }
+document.getElementById("testing").addEventListener("click", async () => {
+  const QRcode = document.getElementById("QRcode").value;
+  datahandler(QRcode);
+});
+const datahandler = (data) => {
+  const regex =
+    /(?<cccd>\d{12})\|(?<cmnd>(\d{9})?)\|(?<name>[^|]+)\|(?<DoB>\d{8})\|(?<sex>(Nam|Nữ))\|(?<address>[^|]+)\|(?<dateOfIssue>(\d{8})?)/gm;
+  let matches;
+  let qrcodes = [];
+  while ((matches = regex.exec(data)) !== null) {
+    if (matches.index === regex.lastIndex) {
+      regex.lastIndex++;
+    }
+    qrcodes.push(matches[0]);
+  }
+  return qrcodes;
+};
